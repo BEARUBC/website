60f6789c3469853850bdd3b7ebe91d626d337805
leticiacid0478 <leticiacid0478@gmail.com>
2024-09-15 13:02:00 -0700

    3D scene now loads, difficulty importing hand

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,10 @@ import * as THREE from 'three';
 //import { OrbitControls } from "react-three/drei/OrbitalControls";
 //import { GLTFLoader } from "three/examples/jsm/Addons.js";
 //import { NodeMaterial } from "three/examples/js/nodes/THREE.Nodes.js";
-import { useGLTF, useAnimations } from '@react-three/drei'
-import Hand from './Hand5.js';
+import { useGraph } from '@react-three/fiber';
+import { useGLTF, useAnimations } from '@react-three/drei';
+import { SkeletonUtils } from 'three-stdlib';
+import hand5 from './Hand5.js';
 import { Canvas, useFrame } from '@react-three/fiber'
 import "./App.css";
 import { Layout } from "./components/Layout";
@@ -13,25 +15,40 @@ import mission from "./Photos/mission.png";
 import vision from "./Photos/vision.png";
 import research from "./Photos/research.png";
 import announcement from "./Photos/announcement.png";
+import { extend } from '@react-three/fiber'
+extend(hand5);
 
 //3D animations done with Blender and Three.js
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// const controls = new OrbitControls( camera, renderer.domElement );
-// const loader = new GLTFLoader();
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+
+function Box(props) {
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={'hotpink'} />
+    </mesh>
+  )
+  }
 
 export const Home = () => {
   return (
     <Layout>
       <Canvas>
-        <THREE.AmbientLight intensity={1}></THREE.AmbientLight>
-        <Hand></Hand>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <hand5 position={[1.2, 0, 0]} />
       </Canvas>
 
 
