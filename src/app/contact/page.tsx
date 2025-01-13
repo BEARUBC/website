021b8214939eb7ab813879a2c17adfd9c3dafae0
farazht <tehranifaraz314@gmail.com>
2025-01-13 09:05:00 -0800

    Fix build

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -50,7 +50,7 @@ const Contact = () => {
                 <p className="text-xs text-secondary tracking-widest">CONTACT US</p>
                 <h2 className="text-3xl md:text-5xl mb-8 tracking-tight">Any Questions?</h2>
                 <p className="text-lg md:text-xl text-foreground p-4 w-full md:w-5/6">
-                    Don&apos;t hesitate to reach out to us directly! Our team will get back to you as soon as possible. Please email us at ubcbionicsteam@gmail.com for urgent inquiries, or if you don't hear back within a few days.
+                    Don&apos;t hesitate to reach out to us directly! Our team will get back to you as soon as possible. Please email us at ubcbionicsteam@gmail.com for urgent inquiries, or if you don&apos;t hear back within a few days.
                 </p>
             </section>
 
